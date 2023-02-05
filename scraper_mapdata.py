fbc7aa84aea90acbb86d097b3086ac4181c7b9d2
Vraj Mashruwala <vrajmashruwala09@gmail.com>
2023-02-05 15:38:03 -0500

    updated logging & added comments

--- scraper_mapdata.py
+++ scraper_mapdata.py
@@ -5,6 +5,7 @@ import os
 import logging
 from dotenv import load_dotenv
 import plotly.graph_objects as go
+import pandas as pd
 
 #load env variables and change logging level to info
 load_dotenv()
@@ -27,14 +28,29 @@ def scrape_nexrad_locations():
     }
     #url used to scrape NEXRAD satellite location data to plot on map
     url = "https://www.ncei.noaa.gov/access/homr/file/nexrad-stations.txt"
-    #recording the response from the webpage
-    response = requests.get(url)
+
+    try:
+        response = requests.get(url)    #recording the response from the webpage
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err_http:
+        logging.error("Exited due to HTTP error while accessing URL")
+        raise SystemExit(err_http)
+    except requests.exceptions.ConnectionError as err_conn:
+        logging.error("Exited due to connection error while accessing URL")
+        raise SystemExit(err_conn)
+    except requests.exceptions.Timeout as err_to:
+        logging.error("Exited due to timeout error while accessing URL")
+        raise SystemExit(err_to)
+    except requests.exceptions.RequestException as err_req:
+        logging.fatal("Exited due to fatal error")
+        raise SystemExit(err_req)
+
     #traverse extracted txt data line by line
     lines = response.text.split('\n')
     for line in lines:
         line=line.strip()   #strip leading and trailing whitespaces
         word_list = line.split(" ")
-        if (word_list[-1].upper() == 'NEXRAD'): #logic to check if the data belongs to a NEXRAD satellite
+        if (word_list[-1].upper() == 'NEXRAD'):     #check if the data belongs to a NEXRAD satellite
             nexrad.append(line)
     nexrad = [i for i in nexrad if 'UNITED STATES' in i]    #only consider satellites over USA
 
@@ -44,7 +60,6 @@ def scrape_nexrad_locations():
         satellite =  [i.strip() for i in satellite if i != ""]
         for i in range(len(satellite)):
             if (re.match(r'\b[A-Z][A-Z]\b',satellite[i].strip())):      #use regex to match with the state field containing 2 capital letters
-                #state_county = satellite[i].split()
                 satellite_metadata['state'].append(satellite[i][:2])    #append state field to final dict
                 satellite_metadata['county'].append(satellite[i][2:])   #append county field to final dict
 
@@ -59,11 +74,15 @@ def scrape_nexrad_locations():
                 satellite_metadata['longitude'].append(satellite[i+1])  #append longtidue as string to final dict
                 satellite_metadata['elevation'].append(int(satellite[i+2]))     #append elevation as int to final dict
                 break
-    
+
+    logging.info("Successfully scrapped NEXRAD statellite location data")
+    sat = pd.DataFrame(satellite_metadata)
+    print(sat)
     return satellite_metadata   #this dict has the final scraped data
 
 def plot_nexrad_locations():
     satellite_metadata = scrape_nexrad_locations()
+    logging.info("Creating plot for NEXRAD statellite locations")
     #plotting the coordinates extracted on a map
     hover_text = []
     for j in range(len(satellite_metadata['county'])):      #building the text to display when hovering over each point on the plot
@@ -110,10 +129,11 @@ def plot_nexrad_locations():
     fig.update_layout(height=700)
     #fig.show()
 
+    logging.info("Successfully created plot for NEXRAD statellite locations")
     return fig
 
 def main():
-    plot = plot_nexrad_locations()
+    map_plot = plot_nexrad_locations()
 
 if __name__ == "__main__":
     logging.info("NEXRAD statellite map data scraper script starts")
